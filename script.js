5a24e1c18c9b54a51eb53ec1ffa78cafa9cb6310
Enzo <jouve.enzo15@gmail.com>
2025-05-11 17:41:44 +0200

    Minor change - color

--- script.js
+++ script.js
@@ -41,10 +41,6 @@ document.querySelectorAll('.color-option').forEach(button => {
                 setTheme({ color1: '153, 102, 204', color2: '255, 229, 180' });
                 break;
 
-            case 'secret':
-                startRainbowTheme();
-                break;
-
             default:
                 console.log("Thème non défini");
         }
@@ -54,27 +50,4 @@ document.querySelectorAll('.color-option').forEach(button => {
 function setTheme(colors) {
     document.documentElement.style.setProperty("--color1", colors.color1);
     document.documentElement.style.setProperty("--color2", colors.color2);
-}
-
-// Lance l’animation arc-en-ciel
-function startRainbowTheme() {
-    let hue = 0;
-    rainbowInterval = setInterval(() => {
-        const c1 = hslToRgb(hue, 100, 50);
-        const c2 = hslToRgb((hue + 180) % 360, 100, 50);
-
-        setTheme({
-            color1: `${c1.r}, ${c1.g}, ${c1.b}`,
-            color2: `${c2.r}, ${c2.g}, ${c2.b}`
-        });
-
-        hue = (hue + 1) % 360;
-    }, 20);
-}
-function hslToRgb(h, s, l) {
-    s /= 100; l /= 100;
-    const k = n => (n + h / 30) % 12;
-    const a = s * Math.min(l, 1 - l);
-    const f = n => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-    return { r: Math.round(255 * f(0)), g: Math.round(255 * f(8)), b: Math.round(255 * f(4)) };
 }
